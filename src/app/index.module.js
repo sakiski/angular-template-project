61c1a201102ea319a4846867084871e2e82d82d3
jinhyun <jinhyun@netmarble.com>
2016-07-05 15:22:23 +0900

    initial commit

---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('angularTemplateProject', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ngRoute', 'ui.bootstrap', 'toastr']);
+
+})();
