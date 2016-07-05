a63590a2fb3c810fe0897f93b65483739cc84b0b
jinhyun <jinhyun@netmarble.com>
2016-07-05 16:35:33 +0900

    bi-chart-review proto typing branch

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -2,6 +2,6 @@
   'use strict';
 
   angular
-    .module('angularTemplateProject', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ngRoute', 'ui.bootstrap', 'toastr']);
+    .module('angularTemplateProject', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ngRoute', 'ui.bootstrap', 'toastr', 'nvd3']);
 
 })();
